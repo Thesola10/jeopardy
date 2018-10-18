7c75b542072329f07449a8a04922aa2161d3804b
TheSola10 <thesola10@bobile.fr>
2018-10-18 09:13:00 +0200

    Slight cleaning on main.c

--- src/main.c
+++ src/main.c
@@ -19,36 +19,43 @@ void loop_reset()
 
 int main(int argc, char *argv[])
 {
-    int pcm;
-    int tmp;
-    int countl;
+    int pcm;      // Exit code for ALSA snd_pcm functions
+    int tmp;      // Temporary variable (See line 64)
+    int buf_size; // The increment to use for buffer feed loops.
+    int countl;   // Count Loops, how many buffer feed loops we need to reach EOF
     int seconds = music_start.wav_size / music_start.byte_rate;
+                  // Number of seconds the wave file will last. Important for 
+                  // loop number calculation
     
+    // Allocate ALSA parameter pointers
     snd_pcm_t *pcm_handle;
     snd_pcm_hw_params_t *params;
     snd_pcm_uframes_t frames;
-    int buf_size;
     
-    
-    if (pcm = snd_pcm_open(&pcm_handle, PCM_DEVICE, 
-                            SND_PCM_STREAM_PLAYBACK, 0) < 0) 
+    // Try and open the default ALSA device
+    if (pcm = snd_pcm_open(&pcm_handle, PCM_DEVICE, SND_PCM_STREAM_PLAYBACK, 0) < 0) 
     {
         printf("ERROR: Could not open device \"%s\": %s",
                                 PCM_DEVICE, snd_strerror(pcm));
     }
     
+    // Retrieve default params
     snd_pcm_hw_params_alloca(&params);
     snd_pcm_hw_params_any(pcm_handle, params);
     
+    // Modify them to fit our needs
     snd_pcm_hw_params_set_access(pcm_handle, params, SND_PCM_ACCESS_RW_INTERLEAVED);
     snd_pcm_hw_params_set_format(pcm_handle, params, SND_PCM_FORMAT_S16_LE);
     snd_pcm_hw_params_set_channels(pcm_handle, params, music_start.num_channels);
     snd_pcm_hw_params_set_rate_near(pcm_handle, params, &music_start.sample_rate, 0);
     
-    // set params
+    // Write params to hardware device
     if ((pcm = snd_pcm_hw_params(pcm_handle, params)) < 0)
+    {
         printf("ERROR: Can't set harware parameters: %s\n", snd_strerror(pcm));
-        
+    }
+    
+    // Determine buffer increment
     snd_pcm_hw_params_get_period_size(params, &frames, 0);
     
     buf_size = frames * music_start.num_channels * 2;
@@ -59,9 +66,11 @@ int main(int argc, char *argv[])
     countl = (seconds * 1000000) / tmp;
         
 #ifdef SIGINT_LOCK
+    // Start blocking SIGINT (^C)
     signal(SIGINT, loop_reset);
 #endif
     
+    // Feed our wav file to ALSA
     for (loops = 0; loops < countl; loops++)
     {
         pcm = snd_pcm_writei(pcm_handle, buf, frames); 
@@ -76,6 +85,7 @@ int main(int argc, char *argv[])
         buf += buf_size;
     }
     
+    // Clean up
     snd_pcm_drain(pcm_handle);
     snd_pcm_close(pcm_handle);
     
